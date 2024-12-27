06f8e212de885f12d829702f7c4395f871364d67
sh-64 <haruya.soccer.64@gmail.com>
2024-12-27 16:33:49 +0900

    Add files via upload

---
+++ kadai_021/event.js
@@ -0,0 +1,9 @@
+const Text =document.getElementById('text');
+
+const Btn =document.getElementById('btn');
+
+Btn.addEventListener('click', () =>{
+    setTimeout(() =>{
+        Text.textContent ='ボタンをクリックしました'
+    }, 2000);
+});
